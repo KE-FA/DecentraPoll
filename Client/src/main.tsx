e9d74cd1e7e09c03d9147b59f0fd17870f1d2c63
NeuraSync <kefakameti@gmail.com>
2026-02-28 16:07:48 +0300

    Modify main.tsx to apply the theme

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, CssBaseline} from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import App from "./App";
+import darkTheme from "./theme"
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
         <Toaster position="top-right" />
       </BrowserRouter>
     </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
+  
 );
 
 
