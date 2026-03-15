b5ed2382181603a34b479ff4d034e4deec81c87b
NeuraSync <kefakameti@gmail.com>
2026-03-15 22:22:32 +0300

    Modify file

--- Client/src/web3/contract.ts
+++ Client/src/web3/contract.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import abi from "./abi.json";
 
-const CONTRACT_ADDRESS = "0x588758d8a0Ad1162A6294f3C274753137E664aE0";
+const CONTRACT_ADDRESS = "0x55548609a728DB2953Aed74E1d470e56433F02D1";
 
 export const getContract = (signer: ethers.Signer) => {
   return new ethers.Contract(CONTRACT_ADDRESS, abi.abi, signer);
